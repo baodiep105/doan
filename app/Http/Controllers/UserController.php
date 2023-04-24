141d3a05640f24a4cdeb11180f094e7ddca68368
baodiep@105 <diephuynhbaobao1@gmail.com>
2023-04-24 20:07:52 +0700

    socialite

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,10 +44,10 @@ class UserController extends Controller
 
     public function callback()
     {
-                $token = Config::get('services.facebook.client_id');
-                $secret = Config::get('services.facebook.client_secret');
-                $social_user = Socialite::driver('facebook')->userFromTokenAndSecret($token, $secret);
-        // $social_user = Socialite::driver('facebook')->stateless()->user();
+                // $token = Config::get('services.facebook.client_id');
+                // $secret = Config::get('services.facebook.client_secret');
+                // $social_user = Socialite::driver('facebook')->userFromTokenAndSecret($token, $secret);
+        $social_user = Socialite::driver('facebook')->stateless()->user();
         dd($social_user);
         if (!is_null($social_user) || !empty($social_user)) {
             $user = User::where('email', $googleUser->email)->where('id_loai', 2)->first();
