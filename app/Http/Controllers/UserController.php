34843d275cfd2f34d2c7dd30a80dfce0b3c8cae9
banni <diephuynhbaobao1@gmail.com>
2023-04-29 00:41:37 +0700

    social

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -97,26 +97,26 @@ class UserController extends Controller {
                 'token' => $token,
             ];
             $user=User::where('email', $social_user['email'])->where('id_loai',2)->first();
-            return 1;
+            if (!$user) {
+                $user = User::create( [ 'username' => $social_user[ 'name' ], 'email' => $social_user[ 'email' ], 'id_loai' => 2, 'is_email' => 1 ] );
+                return $user;
+                // $success[ 'token' ] = $user->createToken( 'myApp' )->accessToken->token;
+                return response()->json( [
+                    'status' => 'success',
+                    'token' => $success,
+                    'user' => $user,
+                ] );
             // return json_encode($social_user);
             // $user = User::where('email', $social_user['email'])->where('id_loai',2)->first();
             // // dd($social_user);
-            // if ($user) {
-            //     // $success[ 'token' ] = $user->createToken( 'myApp' )->accessToken->token;
-            //     return response()->json( [
-            //         'status' => 'success',
-            //         'token' => $success,
-            //         'user' => $user,
-            //     ] );
             // } else {
-            //     $user = User::create( [ 'username' => $social_user[ 'name' ], 'email' => $social_user[ 'email' ], 'id_loai' => 2, 'is_email' => 1 ] );
             //     $success[ 'token' ] = $user->createToken( 'myApp' )->accessToken->token;
             //     return response()->json( [
             //         'status' => 'success',
             //         'user' => $user,
             //         'token' => $success,
             //     ] );
-            // }
+            }
         }
     }
 
